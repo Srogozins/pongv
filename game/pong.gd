61ac3d5858b694dfba3b1d9d8850218d9e234b0e
Staņislavs Rogozins <stanislavs.rogozins@gmail.com>
2016-02-16 02:24:05 +0200

    Rewrite controls

--- game/pong.gd
+++ game/pong.gd
@@ -1,69 +1,87 @@
 extends Node2D
 
+const START_BALL_SPEED = 150
+const START_BALL_DIRECTION = Vector2(-1, 0)
+const PAD_SPEED = 300 # For digital controls
+
+var ball_speed = START_BALL_SPEED
+var ball_direction = START_BALL_DIRECTION
 var screen_size
 var pad_size
+var mouse_controlled
+var left_pad
+var right_pad
+
 
 func _ready():
 	screen_size = get_viewport_rect().size
-	pad_size = get_node("left_player").get_texture().get_size()
+	pad_size = get_node("left_pad").get_texture().get_size() # TODO: Check sizes for pads individually
+
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	left_pad = get_node("left_pad")
+	right_pad = get_node("right_pad")
+	mouse_controlled = [left_pad, right_pad]
+
 	set_process(true)
 	set_process_input(true)
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func process_pad_move(pad, delta_y):
+	var pad_pos = pad.get_pos()
+	pad_pos.y = clamp(pad_pos.y + delta_y, 0, screen_size.y)
+	pad.set_pos(pad_pos)
+
+func process_pad_mode_up(pad, delta):
+	process_pad_move(pad, -PAD_SPEED*delta)
+
+func process_pad_mode_down(pad, delta):
+	process_pad_move(pad, PAD_SPEED*delta)
 
 func _input(ev):
-	# Mouse movement
+	# Mouse-controlled movement
 	if (ev.type==InputEvent.MOUSE_MOTION):
-		var right_pos = get_node("right_player").get_pos()
-		right_pos.y += ev.relative_y
-		get_node("right_player").set_pos(right_pos)
+		for pad in mouse_controlled:
+			var pad_pos = pad.get_pos()
+			pad_pos.y += ev.relative_y
+			pad_pos.y = clamp(pad_pos.y, 0, screen_size.y)
+			pad.set_pos(pad_pos)
 
-# Ball speed px/sec
-var ball_speed = 150
 
-# Ball direction normal vector
-var direction = Vector2(-1, 0)
-
-# Pad speed px/sec
-const PAD_SPEED = 300
+func process_keys(delta):
+	# Pad movement
+	if Input.is_action_pressed("left_move_up"):
+		process_pad_move(left_pad, -PAD_SPEED*delta)
+	if Input.is_action_pressed("left_move_down"):
+		process_pad_move(left_pad, PAD_SPEED*delta)
+	if Input.is_action_pressed("right_move_up"):
+		process_pad_move(right_pad, -PAD_SPEED*delta)
+	if Input.is_action_pressed("right_move_down"):
+		process_pad_move(right_pad, PAD_SPEED*delta)
 
 func _process(delta):
 	var ball_pos = get_node("ball").get_pos()
-	var left_rect = Rect2(get_node("left_player").get_pos() - pad_size/2, pad_size)
-	var right_rect = Rect2(get_node("right_player").get_pos() - pad_size/2, pad_size)
-	ball_pos += direction*ball_speed*delta
+	var left_rect = Rect2(get_node("left_pad").get_pos() - pad_size/2, pad_size)
+	var right_rect = Rect2(get_node("right_pad").get_pos() - pad_size/2, pad_size)
+	ball_pos += ball_direction*ball_speed*delta
 	
 	# Floor/Ceiling collision
-	if ( (ball_pos.y<0 and direction.y <0)
-		or (ball_pos.y>screen_size.y and direction.y>0)):
-			direction.y = -direction.y
+	if ( (ball_pos.y<0 and ball_direction.y <0)
+		or (ball_pos.y>screen_size.y and ball_direction.y>0)):
+			ball_direction.y = -ball_direction.y
 	
 	# Pad collision
-	if ((left_rect.has_point(ball_pos) and direction.x < 0)
-		or (right_rect.has_point(ball_pos) and direction.x > 0)):
-			direction.x = -direction.x
+	if ((left_rect.has_point(ball_pos) and ball_direction.x < 0)
+		or (right_rect.has_point(ball_pos) and ball_direction.x > 0)):
+			ball_direction.x = -ball_direction.x
 			ball_speed *= 1.1
-			direction.y = randf() * 2.0-1
-			direction = direction.normalized()
+			ball_direction.y = randf() * 2.0-1
+			ball_direction = ball_direction.normalized()
 
 	# Wall collision
-	if (ball_pos.x<0 or ball_pos.x > screen_size.x):
+	if (ball_pos.x < 0 or ball_pos.x > screen_size.x):
 		ball_pos = screen_size * 0.5 # move to screen center
 		ball_speed = 150
-		direction = Vector2(-1,0)
+		ball_direction = Vector2(-1,0)
 	
 	get_node("ball").set_pos(ball_pos)
+	process_keys(delta)
 	
-	# Pad movement
-	var left_pos = get_node("left_player").get_pos()
-	if (left_pos.y > 0 and Input.is_action_pressed("left_move_up")):
-		left_pos.y += -PAD_SPEED*delta
-	if (left_pos.y < screen_size.y and Input.is_action_pressed("left_move_down")):
-		left_pos.y += PAD_SPEED*delta
-	get_node("left_player").set_pos(left_pos)
-	
-	var right_pos = get_node("right_player").get_pos()
-	if (right_pos.y > 0 and Input.is_action_pressed("right_move_up")):
-		right_pos.y += -PAD_SPEED*delta
-	if (right_pos.y < screen_size.y and Input.is_action_pressed("right_move_down")):
-		right_pos.y += PAD_SPEED*delta
-	get_node("right_player").set_pos(right_pos)
